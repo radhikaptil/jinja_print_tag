4cab7e2dca7a90c34546c92937adb3fcf6e8f35f
radhikaptil <patilradhika2001@gmail.com>
2023-07-28 15:30:38 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -4,3 +4,8 @@ from django.shortcuts import render
 def data_render(request):
     d={'name':'Vruta','age':14}
     return render(request,'data_render.html',context=d)
+def condition(request):
+    d={'a':15,'b':10}
+    return render(request,'condition.html',context=d)
+
+
