0bd8326a894de1bb78c713833dc200ca4a78c813
radhikaptil <patilradhika2001@gmail.com>
2023-07-28 15:46:03 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -5,7 +5,8 @@ def data_render(request):
     d={'name':'Vruta','age':14}
     return render(request,'data_render.html',context=d)
 def condition(request):
-    d={'a':15,'b':10}
+    d={'a':15,'b':10,'c':80}
     return render(request,'condition.html',context=d)
+
 
 
